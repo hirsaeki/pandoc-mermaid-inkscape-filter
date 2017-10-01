ea2a398b0d5c2e3fad0ba30fa0264dc7f76f58bf
Lars Wilhelmsen <lars@sral.org>
2017-10-01 13:45:43 +0200

    Adding some code

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const pf = require('pandoc-filter');
 const shx = require('shelljs');
-const CodeBlock = pf.CodeBlock;
+const tmp = require('tmp');
+const rm = require('rimraf');
+const mermaid = require('mermaid/src/mermaidAPI').default;
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM(`<!DOCTYPE html>`);
+const $ = require('jquery')(window);
 
-shx.
-console.log(pf);
-console.log('hello world');
+const mermaidInkscapeFilter = (type, value) => {
+    // Not a CodeBlock -> skip.
+    if (type !== 'CodeBlock') return null;
+
+    const cls = value[0][1]; // TODO find out the structure behind this.
+    // Not a mermaid code block -> skip.
+    if (0 > cls.indexOf('mermaid')) return null;
+
+    const code = value[1];
+   
+    mermaid.initialize({
+            startOnLoad:true
+        });
+        $(function(){
+              var cb = function(svgGraph) {
+                const mfile = tmp.fileSync({ postfix: '.svg' });
+               fs.writeSync(mfile, code);
+              };
+            mermaidAPI.render('id1',code,cb);
+        );
+
+    return pf.Div(['',[],[]], [
+        pf.Image(['image.png',[],[]],[])
+    ]);
+}
+
+pf.toJSONFilter(mermaidInkscapeFilter);
